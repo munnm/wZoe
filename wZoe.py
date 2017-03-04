b249ded4fbeec301c90cf5745c73e22ab8717db9
Michael Munn <mikemunn@gmail.com>
2017-03-04 16:58:56 -0500

    minor edits

--- wZoe.py
+++ wZoe.py
@@ -1,4 +1,8 @@
+#useful resource shttps://ozgur.github.io/python-linkedin/
+
 from linkedin import linkedin
+
+
 
 #taken from linkedin app
 API_KEY = '77nymncwcfk86e'
@@ -11,3 +15,4 @@ RETURN_URL = 'http://127.0.0.1:8080/'
 authentication = linkedin.LinkedInAuthentication(API_KEY, API_SECRET, RETURN_URL, linkedin.PERMISSIONS.enums.values())
 print authentication.authorization_url  # open this url on your browser
 application = linkedin.LinkedInApplication(authentication)
+
